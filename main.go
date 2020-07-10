35ff0bc47aec2ec0ca03289ed1035e533b5d9bc9
Jose C <josecarvalholdjr@gmail.com>
2020-07-10 20:58:18 -0300

    Cloudbuild file

--- main.go
+++ main.go
@@ -6,8 +6,8 @@ import (
 
 func main() {
 
-	var valueA = 23
-	var valueB = 62
+	var valueA = 5
+	var valueB = 5
 
 	sumResult := sum(valueA, valueB)
 	fmt.Printf("O resultado da soma de %v e %v é de %v", valueA, valueB, sumResult)
